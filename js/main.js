55f95a0987510e722519beee00266b030a1aa1e4
Stefano Corra <stefano.corra@hotmail.com>
2018-02-11 18:34:48 -0500

    fixed buttons

--- js/main.js
+++ js/main.js
@@ -1,18 +1,30 @@
 
 //Slider range jquery-ui
 $( function() {
-    $( "#slider-range" ).slider({
+    $( "#slider-price" ).slider({
       range: true,
       min: 0,
-      max: 500,
+      max: 1000,
       values: [ 75, 300 ],
       slide: function( event, ui ) {
         $( "#min-price" ).val( ui.values[ 0 ]  );
         $( "#max-price" ).val( ui.values[ 1 ]  );
       }
     });
-    $( "#min-price" ).val(  $( "#slider-range" ).slider( "values", 0 ) );
-    $( "#max-price" ).val(  $( "#slider-range" ).slider( "values", 1 ) );
+    $( "#min-price" ).val(  $( "#slider-price" ).slider( "values", 0 ) );
+    $( "#max-price" ).val(  $( "#slider-price" ).slider( "values", 1 ) );
 
-  
+    /* Slider Bedroom */
+    $( "#slider-bedroom" ).slider({
+      range: true,
+      min: 1,
+      max: 8,
+      values: [ 2, 4 ],
+      slide: function( event, ui ) {
+        $( "#min-bedroom" ).val( ui.values[ 0 ]  );
+        $( "#max-bedroom" ).val( ui.values[ 1 ]  );
+      }
+    });
+    $( "#min-bedroom" ).val(  $( "#slider-bedroom" ).slider( "values", 0 ) );
+    $( "#max-bedroom" ).val(  $( "#slider-bedroom" ).slider( "values", 1 ) );
   } );
