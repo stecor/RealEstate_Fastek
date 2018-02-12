b0863ab3fc287f935496f39e407ced4d091f4e7a
Stefano Corra <stefano.corra@hotmail.com>
2018-02-11 23:13:10 -0500

    improving filters

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ $( function() {
     $( "#slider-price" ).slider({
       range: true,
       min: 0,
-      max: 1000,
-      values: [ 75, 300 ],
+      max: 10000,
+      values: [ 1500, 5000 ],
       slide: function( event, ui ) {
         $( "#min-price" ).val( ui.values[ 0 ]  );
         $( "#max-price" ).val( ui.values[ 1 ]  );
@@ -14,17 +14,27 @@ $( function() {
     $( "#min-price" ).val(  $( "#slider-price" ).slider( "values", 0 ) );
     $( "#max-price" ).val(  $( "#slider-price" ).slider( "values", 1 ) );
 
-    /* Slider Bedroom */
-    $( "#slider-bedroom" ).slider({
-      range: true,
-      min: 1,
-      max: 8,
-      values: [ 2, 4 ],
-      slide: function( event, ui ) {
-        $( "#min-bedroom" ).val( ui.values[ 0 ]  );
-        $( "#max-bedroom" ).val( ui.values[ 1 ]  );
-      }
-    });
-    $( "#min-bedroom" ).val(  $( "#slider-bedroom" ).slider( "values", 0 ) );
-    $( "#max-bedroom" ).val(  $( "#slider-bedroom" ).slider( "values", 1 ) );
-  } );
+// rent button
+  $("#rent").click(function(){
+       $("#rent").css("color","#fff");
+       $("#rent").css("background","#566062");
+       $("#buy").css("color","#566062");
+       $("#buy").css("background","#fff");
+  });
+
+// buy button
+  $("#buy").click(function(){
+       $("#buy").css("color","white");
+       $("#buy").css("background","#566062");
+       $("#rent").css("color","#566062");
+       $("#rent").css("background","#fff");
+  });
+
+ // date
+ $(document).ready(function() {
+        $("#start_datepicker").datepicker();
+    $("#end_datepicker").datepicker();
+
+  });
+
+});
